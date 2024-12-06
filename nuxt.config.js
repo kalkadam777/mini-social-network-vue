0bcea924b4d7fed86dd28c6702f06be36970f280
Erkebulan <kalkadamov14@gmail.com>
2024-12-07 01:00:19 +0500

    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,45 +1,38 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'Lab4',
+    title: "Lab4",
     htmlAttrs: {
-      lang: 'en'
+      lang: "en",
     },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" },
+      { name: "format-detection", content: "telephone=no" },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-    '~/static/main.css'
-  ],
+  css: ["~/static/main.css"],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-  ],
+  buildModules: [],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-    '@nuxtjs/axios'
-  ],
+  modules: ["@nuxtjs/axios"],
   axios: {
-    baseURL: 'http://localhost:8000/api/', // Replace with your Django backend URL
+    baseURL: "http://localhost:8000/api/", // Replace with your Django backend URL
   },
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
-}
+  target: "static",
+
+  build: {},
+};
